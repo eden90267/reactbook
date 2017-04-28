d4a1d7f5ac95fba2754e1d111eb37a80dca0b752
Eden Liu <eden90267@gmail.com>
2017-04-28 18:16:09 +0800

    learning ch06

---
+++ whinepad-v0.0.1/js/source/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by eden_liu on 2017/4/28.
+ */
+var headers = localStorage.getItem('headers');
+var data = localStorage.getItem('data');
+
+if (!headers) {
+    headers = ['Title', 'Year', 'Rating', 'Comments'];
+    data = [['Test', '2015', '3', 'meh']];
+}
