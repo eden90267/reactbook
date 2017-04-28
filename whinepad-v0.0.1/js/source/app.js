e34501916568cc79fd05b36f21cb770d7bf5b2cd
eden90267 <eden90267@gmail.com>
2017-04-28 23:08:55 +0800

    learning ch06

--- whinepad-v0.0.1/js/source/app.js
+++ whinepad-v0.0.1/js/source/app.js
@@ -1,6 +1,15 @@
 /**
  * Created by eden_liu on 2017/4/28.
  */
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Logo from './components/Logo';
+import Excel from './components/Excel';
+
+
 var headers = localStorage.getItem('headers');
 var data = localStorage.getItem('data');
 
@@ -8,3 +17,13 @@ if (!headers) {
     headers = ['Title', 'Year', 'Rating', 'Comments'];
     data = [['Test', '2015', '3', 'meh']];
 }
+
+ReactDOM.render(
+    <div>
+        <h1>
+            <Logo/> Welcome to Whinepad!
+        </h1>
+        <Excel headers={headers} initialData={data}/>
+    </div>,
+    document.getElementById('pad')
+);
---
+++ whinepad-v0.0.1/js/source/components/Logo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by eden_liu on 2017/4/28.
+ */
+import React from 'react';
+
+class Logo extends React.Component {
+    render() {
+        return <div className="Logo" />
+    }
+}
+
+export default Logo
